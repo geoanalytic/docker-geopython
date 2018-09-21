f16f50d0f5f51bc82ce0a1802cc9b527b087bfdb
David Currie <hunter.currie@gmail.com>
2018-09-21 08:41:44 -0600

    reverted back to alpine for 3.6 - creating new repo for ubuntu versions

--- python3.6/provision.sh
+++ python3.6/provision.sh
@@ -7,13 +7,13 @@ set -e # fail on any error
 # ===================================================================
 GEOS_VERSION=3.6.1
 PROJ_VERSION=4.9.3
-PROJ_DATUMGRID_VERSION=1.7
+PROJ_DATUMGRID_VERSION=1.6
 GDAL_VERSION=2.3.1
 
 
 # Install libraries
 # ===================================================================
-RUN apt-get update && apt-get install -y  linux-headers build-base unzip libspatialite-dev libspatialite5 libsqlite3-dev
+apk add --no-cache linux-headers build-base
 
 
 # Install geos
@@ -31,11 +31,10 @@ make -s install
 # ===================================================================
 cd /tmp
 wget http://download.osgeo.org/proj/proj-${PROJ_VERSION}.tar.gz
-wget http://download.osgeo.org/proj/proj-datumgrid-${PROJ_DATUMGRID_VERSION}.tar.gz
+wget http://download.osgeo.org/proj/proj-datumgrid-${PROJ_DATUMGRID_VERSION}.zip
 tar xzf proj-${PROJ_VERSION}.tar.gz
 cd proj-${PROJ_VERSION}/nad
-tar xzf ../../proj-datumgrid-${PROJ_DATUMGRID_VERSION}.tar.gz
-# unzip ../../proj-datumgrid-${PROJ_DATUMGRID_VERSION}.zip
+unzip ../../proj-datumgrid-${PROJ_DATUMGRID_VERSION}.zip
 cd ..
 ./configure --enable-silent-rules
 make -s
